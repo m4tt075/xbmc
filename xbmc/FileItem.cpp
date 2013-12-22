97b9ff60d7dfb1eb524489f469dd4910edecfe6a
montellese <montellese@xbmc.org>
2013-12-23 00:37:06 +0100

    [CFileItemList] add copy constructor

--- xbmc/FileItem.cpp
+++ xbmc/FileItem.cpp
@@ -1936,6 +1936,11 @@ CFileItemList::CFileItemList(const std::string& strPath)
 {
 }
 
+CFileItemList::CFileItemList(const CFileItemList &other)
+{
+  Copy(other, true);
+}
+
 CFileItemList::~CFileItemList()
 {
   Clear();
